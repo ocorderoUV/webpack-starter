967c92b6544db57f515ce186b59c107b9121166c
ocorderoUV <ocordero@unitedvirtualities.com>
2020-01-23 14:56:08 -0700

    JS fundamentals

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,45 @@
-import '../styles/index.scss';
+ let form = document.getElementById('user-form');
 
-console.log('webpack starterkit');
+ form.addEventListener('submit',event=> {
+    let user = form.elements['user'];
+    let userError = document.getElementById('user-error');
+   
+    userError.textContent = 'Invalid Entry';
+    userError.style.color = 'red';
+    user.style.borderColor = 'red';
+    user.focus();
+    
+    event.preventDefault();
+ });
+
+//JQUERY
+/* 
+import $ from 'jquery';
+
+let user ={
+    name: 'Ximena',
+    avatar: 'ximena.jpg'
+};
+
+let promise = $.post("http://5e29ef4e92edd600140de144.mockapi.io/api/v1/users/users",user);
+
+promise.then(
+    data => console.log('success: ', data),
+    error => console.log('error: ',error)
+);
+*/
+
+// HTTP
+/* 
+let xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 &&  this.status == 200){
+        console.log(this.responseText);
+    }
+};
+
+xhttp.open("GET", "http://5e29ef4e92edd600140de144.mockapi.io/api/v1/users/users","true");
+
+xhttp.send();
+*/
